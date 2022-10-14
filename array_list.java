0b04c6cb02034cfccd17b78dfe88d8426d28f020
Akeem <akeem.mighty@gmail.com>
2022-10-13 23:18:43 -0400

    Imported ArrayList and Collections Class + Initialized books ArrayList

--- array_list.java
+++ array_list.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
     public static void main (String[] args){
+        ArrayList<String> books = new ArrayList<String>();
 
     }
 }
