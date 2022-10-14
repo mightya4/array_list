c97784b6ef936efaf5866e271887a062c45c0deb
Akeem <akeem.mighty@gmail.com>
2022-10-13 23:24:15 -0400

    Updated Class + Added Book To Books ArrayList

--- array_list.java
+++ array_list.java
@@ -1,9 +1,13 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Main {
+public class array_list {
     public static void main (String[] args){
         ArrayList<String> books = new ArrayList<String>();
-
+        books.add("To Kill a Mockingbird");
+        books.add("The Da Vinci Code");
+        books.add("The Hunger Games");
+        books.add("Water for Elephants");
+        books.add("Lord of the Flies");
     }
 }
